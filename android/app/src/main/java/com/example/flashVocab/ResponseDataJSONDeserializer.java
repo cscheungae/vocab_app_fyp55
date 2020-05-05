898a7bf6d3197f556ad8a7e532384bb1d5d28d62
KHWan <c760c999@hotmail.com>
2020-05-05 23:55:39 +0800

    Now include short definition and crossreference of the word
    to give hints about the word form,brief explanation,etc. Note, those will not be shown upon preparing and may not work on some of the headwords.

--- android/app/src/main/java/com/example/flashVocab/ResponseDataJSONDeserializer.java
+++ android/app/src/main/java/com/example/flashVocab/ResponseDataJSONDeserializer.java
@@ -3,6 +3,7 @@ package com.example.flashVocab;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonArray;
 
@@ -48,26 +49,43 @@ public class ResponseDataJSONDeserializer implements JsonDeserializer<ResponseDa
                         JsonArray entries = LexicalEntry.getAsJsonObject().getAsJsonArray("entries");
                         if(entries!=null) {
                             entries.forEach(entry -> {
-                                WordDefinitions def = new WordDefinitions();
-                                def.setPartOfSpeech(pos);
-
 
 
                                 JsonArray senses = entry.getAsJsonObject().getAsJsonArray("senses");
-                                senses.forEach(sense -> {
-                                    def.setDefinition(sense.getAsJsonObject().getAsJsonArray("definitions").get(0).getAsString());
-                                    List<String> examples = new ArrayList<String>();
-                                    JsonArray rsp_examples = sense.getAsJsonObject().getAsJsonArray("examples");
+                                if(senses!=null) {
 
-                                    if(rsp_examples!=null)
-                                    rsp_examples.forEach(example -> {
-                                        examples.add(example.getAsJsonObject().get("text").getAsString());
+                                    senses.forEach(sense -> {
+                                        WordDefinitions def = new WordDefinitions();
+                                        def.setPartOfSpeech(pos);
+                                        if(sense.getAsJsonObject().get("definitions")!=null){
+                                            def.setDefinition(sense.getAsJsonObject().getAsJsonArray("definitions").get(0).getAsString());
+                                            List<String> examples = new ArrayList<String>();
+                                            JsonArray rsp_examples = sense.getAsJsonObject().getAsJsonArray("examples");
+
+                                            if (rsp_examples != null)
+                                                rsp_examples.forEach(example -> {
+                                                    examples.add(example.getAsJsonObject().get("text").getAsString());
+                                                });
+
+                                            def.setExamples(examples);
+                                        }
+                                        else if(sense.getAsJsonObject().get("shortDefinitions")!=null){
+                                            def.setDefinition(sense.getAsJsonObject().get("shortDefinitions").getAsJsonArray().get(0).getAsString());
+                                        }
+                                        //get the cross reference (other forms of the word)
+                                        else if(sense.getAsJsonObject().get("crossReferences")!=null){
+                                            def.setDefinition(sense.getAsJsonObject().get("crossReferenceMarkers").getAsJsonArray().get(0).getAsString());
+                                        }
+
+                                        //get the definition
+
+                                        else
+                                            def = null;
+                                        if(def!=null)
+                                            defs.add(def);
                                     });
 
-                                    def.setExamples(examples);
-                                });
-                                defs.add(def);
-                            });
+                                }});
                         }
                     });
                 }
@@ -78,7 +96,12 @@ public class ResponseDataJSONDeserializer implements JsonDeserializer<ResponseDa
 
         } catch(NullPointerException ex){
             ex.printStackTrace();
+            return null;
+        } catch(Exception ex){
+            ex.printStackTrace();
+            return null;
         }
+
         return null;
     }
 }
