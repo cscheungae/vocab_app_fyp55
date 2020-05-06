60d58ef333bf35f62dab273b6113d3a547da91f6
KHWan <c760c999@hotmail.com>
2020-05-06 22:55:03 +0800

    Schedules notification at the time user need to study the words

--- android/app/src/main/java/Services/NotifyStudyTimeService.java
+++ android/app/src/main/java/Services/NotifyStudyTimeService.java
@@ -1,9 +1,12 @@
 package Services;
 
+import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.example.flashVocab.MainActivity;
+import com.example.flashVocab.R;
 
 public class NotifyStudyTimeService extends Worker {
     NotifyStudyTimeService(Context context, WorkerParameters params){
@@ -20,18 +26,30 @@ public class NotifyStudyTimeService extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        String date =  getInputData().getString("date");
-        Log.i("NotifyStudyTimeService",date);
+        try {
+            String date = getInputData().getString("date");
+            Log.i("NotifyStudyTimeService", date);
 
-        Notification notification = new NotificationCompat.Builder(getApplicationContext(),"12h3k123")
-                .setContentTitle("Hi")
-                .setContentText("Time is: "+ date)
-                .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-                .build();
-        NotificationManagerCompat.from(getApplicationContext()).notify(2389283,notification);
-        return Result.success();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("route", "/study");
+            intent.setAction(Intent.ACTION_RUN);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+            Notification notification = new NotificationCompat.Builder(getApplicationContext(), "12h3k123")
+                    .setContentTitle("Time to study")
+                    .setContentText("Revise the words you have learnt with FlashVocab!")
+                    .setSmallIcon(R.mipmap.flashvocab_ic_launcher)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .build();
+            NotificationManagerCompat.from(getApplicationContext()).notify(2389283, notification);
+            return Result.success();
+        }catch(Exception e){
+            e.printStackTrace();
+            return Result.failure();
+        }
     }
 
 
